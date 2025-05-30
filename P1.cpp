be36a2a36dfb779c86ea59a105c03ff75671286b
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-29 23:59:44 -0500

    Actualiza ejercicio 1 de estructuras repetitivas: mejoramiento de interfaz

--- P1.cpp
+++ P1.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main() {
     float n1, n2, n3, promedio;
-    cout << "Ingrese las 3 calificaciones: "<< endl;
+    cout << "Ingrese las 3 calificaciones par verificar si el alumno esta aprobado o reprobado: "<< endl;
+    cout << "Su promedio de tres calificaciones debe ser mayor o igual a 70 para aprobar; "<< endl;
+    cout << "De otro modo estara desaprobado; "<< endl;
+
     cin >> n1 >> n2 >> n3;
 
     promedio = (n1 + n2 + n3) / 3;
 
     if(promedio >= 70) {
-        cout << "Alumno aprobado" << endl;
+        cout << "El siguiente alumno esta aprobado" << endl;
     } else {
-        cout << "Alumno reprobado" << endl;
+        cout << "Elsiguiente alumno esta reprobado" << endl;
     }
 
     return 0;
