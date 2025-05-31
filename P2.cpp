d6c5dad0cc23b97bcaf29a4e61770d7075c697cb
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-30 23:42:01 -0500

    Actualiza ejercicio 2 de estructuras selectivas: mejoramiento de interfaz para el usuario

--- P2.cpp
+++ P2.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
-    float compra, total;
-    cout << "Ingrese el monto de la compra: ";
+    float compra,descuento, total;
+    cout << "REALIZAMOS UN DESCUENTO DEL 20% CUYA COMPRA SUPERE LOS S/.1000 "<<endl;
+    cout << "Ingrese el monto de la compra: "<<endl;
     cin >> compra;
 
     if(compra > 1000) {
-        total = compra * 0.8; 
+        total = compra * 0.8;
+		descuento=compra*0.2; 
     } else {
         total = compra;
     }
-
+	cout<<"RECIBO DE COMPRA "<<endl;
+	cout<<"Monto de la compra: "<<compra<<endl;
+	cout<<"Descuento del 20%: "<<descuento<<endl;
     cout << "Total a pagar: S/." << total << endl;
     return 0;
 }
