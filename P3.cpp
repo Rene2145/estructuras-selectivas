157a2e4edeee8e3e75d0026e59d9066d2de1c540
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-31 00:00:14 -0500

    Actualiza ejercicio 3 de estructuras selectivas: mejoramiento de interfaz y aplicacion

--- P3.cpp
+++ P3.cpp
@@ -3,21 +3,33 @@ using namespace std;
 
 int main() {
     int cantidad;
-    float precio, total;
-    cout << "Ingrese el precio de la camisa: ";
+    float precio,descuento, total;
+    cout<<"DESCUENTOS"<<endl;
+    cout<<"10% SI COMPRAN HASTA 2 CAMISAS"<<endl;
+    cout<<"20% SI COMPRAN 3 O MAS CAMISAS"<<endl;
+    cout << "Ingrese el precio para la camisa: "<<endl;
     cin >> precio;
-    cout << "Ingrese la cantidad de camisas: ";
+    cout << "Ingrese la cantidad de camisas que va a comprar: "<<endl;
     cin >> cantidad;
 
     total = precio * cantidad;
 
     if(cantidad >= 3) {
-        total *= 0.8; 
-    } else {
-        total *= 0.9; 
-    }
+        total=total*0.8;
+        descuento=total*0.2; 
+    	cout<<"RECIBO DE COMPRA "<<endl;
+		cout<<"Monto de la compra: "<<precio<<endl;
+		cout<<"Descuento del 20%: "<<descuento<<endl;
+		cout << "Total a pagar: S/." << total << endl;
 
-    cout << "Total a pagar: S/." << total << endl;
+    } else {
+        total=total*0.9; 
+        descuento=total*0.1; 
+    	cout<<"RECIBO DE COMPRA "<<endl;
+		cout<<"Monto de la compra: "<<precio<<endl;
+		cout<<"Descuento del 10%: "<<descuento<<endl;
+        cout << "Total a pagar: S/." << total << endl;
+    }
     return 0;
 }
 
