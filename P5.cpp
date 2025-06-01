10020c6b3a8fcd224a6b8748971072918eff9432
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-01 17:23:56 -0500

    Actualiza ejercicio 5 de estructuras selectivas: validación agregada al no permitir numeros menores o iguales a 0 y mejoramiento de interfaz para el usuario

--- P5.cpp
+++ P5.cpp
@@ -4,20 +4,29 @@ using namespace std;
 int main() {
     int edad;
     char sexo;
-    float pulsaciones;
-
-    cout << "Ingrese su edad: ";
+    int pulsaciones;
+    cout<<">>>==============================<<<"<<endl;
+	cout<<"          AEROBISLUD 10S"<<endl;
+	cout<<"         monitor de pulso"<<endl;
+	cout<<">>>==============================<<<"<<endl;
+	cout<<""<<endl;
+    cout << "Porfavor, ingrese su edad: "<<endl;
     cin >> edad;
-    cout << "Ingrese su sexo (M/F): ";
-    cin >> sexo;
-
-    if(sexo == 'F' || sexo == 'f') {
-        pulsaciones = (220 - edad) / 10.0;
-    } else {
-        pulsaciones = (210 - edad) / 10.0;
-    }
-
-    cout << "Pulsaciones cada 10 segundos: " << pulsaciones << endl;
+    if (edad <= 0) {
+        cout << "¡Tu edad es incorrecta!"<<endl<<"No podemos calcular tus pulsaciones."<< endl;
+        }else{
+        	cout << "Ahora, ingrese su sexo: "<<endl;
+    		cout<<"1 <- Masculino"<<endl;
+    		cout<<"2 <- Femenino"<<endl;
+    		cin >> sexo;
+    		if( sexo == '1') {
+        	pulsaciones = (220 - edad) / 10.0;
+        	cout << "¡Listo, amigo! Considerando tu edad el numero de pulsaciones que debes alcanzar es de " << pulsaciones <<"."<<endl;
+   			} else {
+        		pulsaciones = (210 - edad) / 10.0;
+        	cout << "Muy bien amiga, Considerando tu edad el numero de pulsaciones que debes alcanzar es de " << pulsaciones <<"."<<endl;
+    		}    
+		}
     
     return 0;
 }
