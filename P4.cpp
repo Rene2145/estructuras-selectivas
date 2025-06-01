dbb988de3da903e946f9c5438817fc4a5185b637
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-31 22:35:49 -0500

    Actualiza ejercicio 4 de estructuras selectivas: validacion de no pormetir numeros negativos y mejoramiento de interfaz para el usuario

--- P4.cpp
+++ P4.cpp
@@ -4,20 +4,34 @@ using namespace std;
 int main() {
     float compra, descuento;
     int numero;
+    cout << "+--------------------------+"<<endl;
+    cout << "|  PROMOCION & DESCUENTOS  |"<<endl;
+	cout << "|   PARA TODO EL PUBLICO   |"<<endl; 
+    cout << "+--------------------------+"<<endl;
+    cout << ""<<endl;
     
-    cout << "Ingrese el total de la compra: ";
+    cout << "Ingresa el total de tu compra: "<<endl;
     cin >> compra;
-    cout << "Ingrese el número (azar): ";
-    cin >> numero;
 
+	if(compra<=0){
+		cout<<"¡Monto invalido!"<<endl;
+		cout<<"Perdiste tu descuento.";
+		return 0;
+	}
+	cout <<"Bien, ahora para poder participar"<<endl;
+	cout <<"debes ingresar un numero del 1 al 100: "<<endl;
+    cin >> numero;
+    
     if(numero < 74) {
         descuento = compra * 0.15;
+    cout<<"¡FELICIDADES!"<<endl;
+    cout<<"Descuento del 15% obtenido: " << endl;
     } else {
         descuento = compra * 0.20;
+    cout<<"¡FELICIDADES!";
+    cout<<"Descuento del 20% obtenido: " << endl;
     }
-
-    cout << "Descuento obtenido: S/." << descuento << endl;
-    cout << "Total a pagar: S/." << compra - descuento << endl;
+    cout << "Tu compra es de S/." << compra - descuento << endl;
     
     return 0;
 }
