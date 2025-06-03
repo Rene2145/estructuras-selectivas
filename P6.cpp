d2f3fa15e78d12056c33046cc5976ae233718fe1
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-02 21:24:43 -0500

    Actualiza ejercicio 6 de estructuras selectivas: Mejoramiento de interfaz para el usuario

--- P6.cpp
+++ P6.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 int main() {
     float promedio, pension, total;
-    cout << "Ingrese su promedio: ";
+    cout << "$---------------------------------$"<<endl; 
+    cout << "|        ESTIMULO ACADEMICO       | " <<endl;
+    cout << "$---------------------------------$"<<endl<<""<<endl;
+    cout << "Por favor ingrese su promedio general entre 0 y 20: "<<endl;
     cin >> promedio;
-    cout << "Ingrese el costo de la pensión: ";
+    cout << "Ahora ingrese el costo de la pension: "<<endl;
     cin >> pension;
 
     if(promedio >= 18) {
-        total = pension * 0.7; // 30% descuento
+        total = pension * 0.7;
+        cout<<"¡Felicidades! por tu buen promedio obtendras un descuento del 30%"<<endl;
     } else {
-        total = pension * 1.18; // Incluye 18% IGV
+        total = pension * 1.18;
+		        cout<<"¡Se le va a incluir un 18% de IGV al precio total!"<<endl; 
     }
 
-    cout << "Total a pagar: S/." << total << endl;
+    cout << "Por lo que su total a pagar sera de S/. " << total << endl;
     
     return 0;
 }
