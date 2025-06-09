f07cb641a149e3280eb31a2b9d11724a9419ab23
Josue Adrian Sosa Cruz <jasosac@unjbg.edu.pe>
2025-06-09 10:28:24 -0500

    Mejora en la legibilidad y optimizacion del codigo en los ejercicios 3 y 5 de estructuras selectivas

--- P3.cpp
+++ P3.cpp
@@ -1,35 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     int cantidad;
-    float precio,descuento, total;
-    cout<<"DESCUENTOS"<<endl;
-    cout<<"10% SI COMPRAN HASTA 2 CAMISAS"<<endl;
-    cout<<"20% SI COMPRAN 3 O MAS CAMISAS"<<endl;
-    cout << "Ingrese el precio para la camisa: "<<endl;
+    float precio, descuento, totalBruto, totalFinal;
+    
+    cout << "DESCUENTOS\n";
+    cout << "10% si compra hasta 2 camisas\n";
+    cout << "20% si compra 3 o más camisas\n\n";
+
+    cout << "Ingrese el precio de una camisa: ";
     cin >> precio;
-    cout << "Ingrese la cantidad de camisas que va a comprar: "<<endl;
+
+    cout << "Ingrese la cantidad de camisas a comprar: ";
     cin >> cantidad;
 
-    total = precio * cantidad;
+    totalBruto = precio * cantidad;
 
-    if(cantidad >= 3) {
-        total=total*0.8;
-        descuento=total*0.2; 
-    	cout<<"RECIBO DE COMPRA "<<endl;
-		cout<<"Monto de la compra: "<<precio<<endl;
-		cout<<"Descuento del 20%: "<<descuento<<endl;
-		cout << "Total a pagar: S/." << total << endl;
-
+    if (cantidad >= 3) {
+        descuento = totalBruto * 0.20;
     } else {
-        total=total*0.9; 
-        descuento=total*0.1; 
-    	cout<<"RECIBO DE COMPRA "<<endl;
-		cout<<"Monto de la compra: "<<precio<<endl;
-		cout<<"Descuento del 10%: "<<descuento<<endl;
-        cout << "Total a pagar: S/." << total << endl;
+        descuento = totalBruto * 0.10;
     }
+
+    totalFinal = totalBruto - descuento;
+
+    cout << "\n----- RECIBO DE COMPRA -----\n";
+    cout << "Monto total sin descuento: S/. " << totalBruto << endl;
+    cout << "Descuento aplicado: S/. " << descuento << endl;
+    cout << "Total a pagar: S/. " << totalFinal << endl;
+
     return 0;
 }
-
